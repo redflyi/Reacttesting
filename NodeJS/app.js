29a04669d8ea95bd84ee68f63d8b1a777d3e9fdb
redflyi <1mikehale@googlemail.com>
2021-04-13 00:41:56 +0100

    Next is API

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -7,6 +7,24 @@
 
 // app.listen(3000);
 
+
+const errHandler = err => console.log(err);
+
+const dataHandler = data => console.log(data.toString());
+
 const fs = require('fs');
 const fileName = "target.txt";
-fs.watch(fileName, () => console.log(`File changed!`));
+
+// const data = fs.readFileSync(fileName);
+// console.log(data.toString());
+
+fs.readFile(fileName, (err, data) => {
+    if (err) errHandler(err);
+    dataHandler(data);
+});
+
+console.log('node JS prgraming...');
+
+
+
+//fs.watch(fileName, () => console.log(`File changed!`));
